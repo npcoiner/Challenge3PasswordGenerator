146a129259019a6c1b8468be44c2c4616ada9d0a
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-04-06 19:34:36 -0700

    Added quesitonUserRequirements Function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +12,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+// Add function for asking user for specific password requriements such as:
+function questionUserRequirements{
+// Length between 8 and 128 characters
+// The choice of including lowercase, uppercase, numeric, and/or special characters
+// At least one character type should be selected
+  
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
